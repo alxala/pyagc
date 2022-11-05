38ecd997b0075893b078464caa8d520323ec1f77
alxala <78293441+alxala@users.noreply.github.com>
2022-11-06 04:48:18 +0700

    Update ads.js

--- ads/ads.js
+++ ads/ads.js
@@ -1,17 +1,62 @@
-(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"\natOptions = {\n'key' : '35ff9bfde9c6692e37b4dc39e859a703',\n'format' : 'iframe',\n'height' : 250,\n'width' : 300,\n'params' : {}\n};\n"},{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"https://wimpmustsyllabus.com/35ff9bfde9c6692e37b4dc39e859a703/invoke.js"}],"tag":"script","inner":""}],{"target":[".container","#container",".content"]});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
-(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//wimpmustsyllabus.com/fb/22/84/fb228425ef941d0fe6f01bbf6ec15141.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
-(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//wimpmustsyllabus.com/27/34/38/273438fe4d5fe1b7342dc5c90c399b2e.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+let dbAds=[
+  {
+    "target-selector":[
+      ".card-title",
+      ".siteorigin-widget-tinymce textwidget"
+    ],
+    "position":"out-top", //out-top, out-bottom, in-top, in-bottom
+    "data" :`
+    <script type="text/javascript">
+	    atOptions = {
+		    'key' : '35ff9bfde9c6692e37b4dc39e859a703',
+		    'format' : 'iframe',
+		    'height' : 250,
+		    'width' : 300,
+		    'params' : {}
+	    };
+	    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/35ff9bfde9c6692e37b4dc39e859a703/invoke.js"></scr' + 'ipt>');
+    </script>
+    `,
+    "style":`
+      width: 90%;
+      margin: auto;
+      margin-bottom: 10px;
+      margin-top: 10px;
+    `
+  }
+];
+
+dbAds.forEach(function(a){
+  let createElDom=document.createElement("div");
+  createElDom.setAttribute("style",a["style"]);
+  createElDom.innerHTML=a["data"];
+  let dataScript=[];
+  createElDom.querySelectorAll("script").forEach(function(b){
+    let createElCostom=document.createElement("script");
+    createElCostom.innerHTML=b.innerHTML;
+    dataScript.push(createElCostom);
+    b.remove();
+  });
+  a["target-selector"].forEach(function(b){
+    let targetEl=document.querySelector(b);
+    if(targetEl){
+      if(a["position"]=="out-bottom"){
+        targetEl.parentNode.insertBefore(createElDom,targetEl.nextSibling);
+      }else if(a["position"]=="out-top"){
+        targetEl.parentNode.insertBefore(createElDom,targetEl.nextSibling);
+        createElDom.after(targetEl);
+      }else if(a["position"]=="in-top"){
+
+      }else if(a["position"]=="in-top"){
+        
+      };
+      dataScript.forEach(function(b){
+        createElDom.append(b); 
+      });
+    }else{
+      console.log("target "+a["target-selector"]+" tidak ditemukan"); 
+    };
+  });
+});
 (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"var _Hasync= _Hasync|| [];\n_Hasync.push(['Histats.start', '1,4537605,4,0,0,0,00010000']);\n_Hasync.push(['Histats.fasi', '1']);\n_Hasync.push(['Histats.track_hits', '']);\n(function() {\nvar hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;\nhs.src = ('//s10.histats.com/js15_as.js');\n(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);\n})();"},{"attr":[],"tag":"noscript","inner":"<a href=\"/\" target=\"_blank\"><img src=\"//sstatic1.histats.com/0.gif?4537605&amp;101\" alt=\"\" border=\"0\"></a>"}],{"target":"footer"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
-<!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4537605,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4537605&101" alt="" border="0"></a></noscript>
-<!-- Histats.com  END  -->
